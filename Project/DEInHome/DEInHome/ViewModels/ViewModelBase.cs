e67f15cd605abfb171579c307c0cb043bd8abea5
L0nelys016 <artemka.volkov2017@yandex.ru>
2026-06-01 00:57:28 +0300

    init program

---
+++ Project/DEInHome/DEInHome/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DEInHome.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ Project/DEInHome/DEInHome/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DEInHome.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
